54416a1d242f09cbf72d55fe5de0dfc7242f7075
jinxiaonb <315232021@163.com>
2017-11-10 17:21:02 +0800

    add upload image

--- lexueshuo/pages/apply/apply.js
+++ lexueshuo/pages/apply/apply.js
@@ -5,7 +5,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    upload_picture_list:[{path:"../../images/bg.jpg"}]
   },
 
   /**
@@ -62,5 +62,32 @@ Page({
    */
   onShareAppMessage: function () {
   
+  },
+  uploadpic:function(e){
+    console.log(e);
+    var that = this;
+    var upload_picture_list = that.data.upload_picture_list;
+    wx.chooseImage({
+      count:1,
+      success:function(res){
+        var tempFiles = res.tempFiles;
+        console.log(tempFiles[0]);
+        upload_picture_list.push(tempFiles[0]);
+        // that.setData({
+        //   upload_picture_list:upload_picture_list
+        // });
+        console.log(that.data.upload_picture_list);
+        wx.uploadFile({
+          url: 'https://ssl.xt.cn/lexue.ajax.php?action=imageupload',
+          filePath: tempFiles[0]['path'],
+          name: 'file',
+          formData:{'path':'wxchat'},
+          success:function(res){
+            var data = JSON.parse(res.data);
+            console.log(data);
+          }
+        })
+      }
+    });
   }
 })
